3bccbc204cd86abb01ce60c9ff28f38a964aed0a
sanshan.gan <lucagsscd@gmail.com>
2023-04-07 07:14:25 +0800

    [sanshan] feat: add a random square calculator

---
+++ src/main/kotlin/basic/variable_arithmetic.kt
@@ -0,0 +1,16 @@
+package basic
+
+class variable_arithmetic {
+}
+
+fun main(){
+
+    var Pi= 3.14
+    var radius = Math.random() * 10
+
+    var area = radius * radius * Pi
+
+    println("The area is $area")
+
+
+}
